78f1cd006d9b853b5ab8c2cee9393354baf78e89
Nikola Obradovic <obradovicnik01@gmail.com>
2019-07-06 19:38:10 +0200

    Counter

---
+++ lil Projects/Counter/main.js
@@ -0,0 +1,52 @@
+let counter = document.querySelector('.counter');
+const addCount = document.querySelector('#add-count-btn');
+const lowerCount = document.querySelector('#lower-count-btn');
+
+
+let count = 0;
+
+
+addCount.addEventListener('click', incrementCounter);
+lowerCount.addEventListener('click', decrementCounter);
+
+
+
+
+
+function incrementCounter() {
+  count++;
+  counter.innerHTML = count;
+  if (counter.innerHTML > '0') {
+    counter.style.color = '#4caf50'
+  } else if (counter.innerHTML === '0') {
+    counter.style.color = 'white'
+  }
+  counter.animate([{
+    opacity: '0.2'
+  }, {
+    opacity: '1.0'
+  }], {
+    duration: 500,
+    fill: 'forwards'
+  })
+}
+
+
+
+function decrementCounter() {
+  count--;
+  counter.innerHTML = count;
+  if (counter.innerHTML < '0') {
+    counter.style.color = 'red'
+  } else if (counter.innerHTML === '0') {
+    counter.style.color = 'white'
+  }
+  counter.animate([{
+    opacity: '0.2'
+  }, {
+    opacity: '1.0'
+  }], {
+    duration: 500,
+    fill: 'forwards'
+  })
+}
